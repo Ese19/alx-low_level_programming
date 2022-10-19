5b9c10ca8aed17cdf72a73d8aec3e94ac962e78c
Eseoghene Okoro <enerhen2013@gmail.com>
2022-10-19 03:58:07 -0700

    4th task done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,20 +5,20 @@
  * Return: 0
  */
 
-int main(void) 
+int main(void)
 {
 	char ch;
 
-	for(ch = 'a' ; ch <= 'z' ; ch++)
-       	{
-        	putchar(ch);
-	}
-
-	for(ch = 'A' ; ch <= 'Z' ; ch++) 
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
-        }
+	}
+
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
 
 	putchar('\n');
-	return(0);
+	return (0);
 }
