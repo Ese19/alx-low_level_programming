42f13b5c6ae75dc728873eb9dac1ed87f4041a6c
Eseoghene Okoro <enerhen2013@gmail.com>
2022-10-19 08:47:16 -0700

    9th task done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
