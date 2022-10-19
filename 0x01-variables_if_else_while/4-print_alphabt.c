6337b265e69fc00f61b19f08a00c1d4b1adf5336
Eseoghene Okoro <enerhen2013@gmail.com>
2022-10-19 05:43:17 -0700

    5th task done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the letters except q and e in lowercase
+ * Retrurn: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+		}
+
+		putchar(ch);
+	}
+	
+	putchar('\n');
+	return (0);
+}
