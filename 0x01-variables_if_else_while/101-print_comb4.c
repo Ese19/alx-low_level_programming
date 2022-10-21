c9b02afcda08980adc44bc7af5d91749ffe10a7c
Eseoghene Okoro <enerhen2013@gmail.com>
2022-10-21 07:36:30 -0700

    12th task done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 	
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i+1; i < 10; i++)
+		for (j = i + 1; j < 10; j++)
 		{
-			for (k = j+1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
-				if (i != j && j != k )
+				if (i != j && j != k)
 				{
 					putchar('0' + i);
 					putchar('0' + j);
